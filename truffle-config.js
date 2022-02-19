eff3a56d461fddd4750bed2b90f0e1476704aa65
badalharit <badalharitmrt@gmail.com>
2022-02-19 20:05:22 +0530

    Compiled the solidty contracts with `solidity 0.8.12`

--- truffle-config.js
+++ truffle-config.js
@@ -27,5 +27,19 @@ module.exports = {
         timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
         skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
         }
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.12",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
   }
 };
