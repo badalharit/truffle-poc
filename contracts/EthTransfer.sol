4a492678a667a8e50a8c8a85d859bf61133c7de7
badalharit <badalharitmrt@gmail.com>
2022-02-19 19:54:35 +0530

    Addedd `EthTransfer` Smart Contract | Tested on RxIDE & Validate

---
+++ contracts/EthTransfer.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract EthTransfer{
+    address public sender;
+    address public receiver;
+
+    constructor(){
+        receiver = msg.sender;//Global variable to pass the contract deployer hash address
+    }
+
+    receive() external payable{
+        sender = payable(msg.sender);//It'll transfer the ETH
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance; // Balance in contract address
+    }
+
+    function transferToReceiver() public{
+        payable(receiver).transfer(getBalance());
+    }
+}
