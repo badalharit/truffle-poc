78bc6eef205774b1074c75499b37575622b4e8b2
badalharit <badalharitmrt@gmail.com>
2022-02-19 19:05:39 +0530

    Installed `Hdwallet` for `Rinkeby` & `Ropsten` deployements

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const path = require("path");
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +10,22 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
-    }
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/69682cb3dc1240a2a25d18f02f97cf20`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      },
+      rinkeby: {
+        provider: () => new HDWalletProvider(mnemonic, `wss://rinkeby.infura.io/ws/v3/69682cb3dc1240a2a25d18f02f97cf20`),
+        network_id: 4,       // Rinkeby's id
+        gas: 5500000,        // Rinkeby has a lower block limit than mainnet
+        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+        }
   }
 };
